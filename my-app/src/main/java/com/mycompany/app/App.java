07f87aeff56e19a5dfaf2e6fee490957a0f9a7ab
lisssq <lizynka05@yandex.ru>
2026-05-16 02:00:52 +0300

    format

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -6,26 +6,27 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class App {
+
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "D:\\software testing (ST-2026)\\chromedriver-win64\\chromedriver.exe");
-        
+
         WebDriver webDriver = new ChromeDriver();
-        
+
         try {
             System.out.println("=== Task #1 ===");
             webDriver.get("https://www.calculator.net/password-generator.html");
-            
+
             Thread.sleep(2000);
-            
+
             WebElement passwordElement = webDriver.findElement(By.cssSelector(".verybigtext b"));
             String generatedPassword = passwordElement.getText();
-            
+
             System.out.println("Пароль от сервера: " + generatedPassword);
-            System.out.println(); 
-            
+            System.out.println();
+
             Task2.run(webDriver);
             Task3.run(webDriver);
-            
+
             Thread.sleep(3000);
         } catch (Exception e) {
             System.out.println("Ошибка выполнения Задания №1:");
